31ca50e7c3b7ae0c5679be398a27951cab04b274
Liang <386229680@qq.com>
2025-02-25 23:27:43 +0800

    [48]旋转图像 [73]矩阵置零

---
+++ src/main/java/com/algorithm/leetcode/editor/cn/[73]矩阵置零.java
@@ -0,0 +1,115 @@
+//给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。 
+//
+// 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
+//输出：[[1,0,1],[0,0,0],[1,0,1]]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+//输出：[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[0].length 
+// 1 <= m, n <= 200 
+// -2³¹ <= matrix[i][j] <= 2³¹ - 1 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 一个直观的解决方案是使用 O(mn) 的额外空间，但这并不是一个好的解决方案。 
+// 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。 
+// 你能想出一个仅使用常量空间的解决方案吗？ 
+// 
+//
+// Related Topics 数组 哈希表 矩阵 👍 1160 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) return;
+        int n = matrix[0].length;
+
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        // 检查第一行是否有零
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        // 检查第一列是否有零
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        // 使用第一行和第一列来标记需要置零的行和列
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // 根据标记，将相应的行置零
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // 根据标记，将相应的列置零
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // 如果第一行有零，将第一行全部置零
+        if (firstRowHasZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // 如果第一列有零，将第一列全部置零
+        if (firstColHasZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
